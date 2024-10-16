3f89241c54ff2b171aef5565f01829d40f346f39
wkauan <kauanmbomfim5@gmail.com>
2024-10-16 17:28:37 -0300

    feat: Added validate date time to start and to end

--- src/main/java/br/com/kauan/todolist/task/TaskController.java
+++ src/main/java/br/com/kauan/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.kauan.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,23 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
 
         taskModel.setIdUser((UUID) idUser);
 
+        var currentDate = LocalDateTime.now();
+
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de ínicio / data de término não pode vir antes da data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de ínicio deve vir antes da data de término");
+        }
+
         var task = this.taskRepository.save(taskModel);
 
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
