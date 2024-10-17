476b78c99a283e1b175c4bce39fea38c2902e7f3
wkauan <kauanmbomfim5@gmail.com>
2024-10-17 00:09:06 -0300

    feat: Created error handle to title char length

---
+++ src/main/java/br/com/kauan/todolist/errors/ExceptionHandleController.java
@@ -0,0 +1,15 @@
+package br.com.kauan.todolist.errors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandleController {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(e.getMostSpecificCause().getMessage());
+    }
+}
--- src/main/java/br/com/kauan/todolist/task/TaskModel.java
+++ src/main/java/br/com/kauan/todolist/task/TaskModel.java
@@ -31,4 +31,13 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception {
+
+        if (title.length() > 50) {
+            throw new Exception("O campo titulo deve conter no máximo 50 caracteres");
+        }
+
+        this.title = title;
+    }
+
 }
